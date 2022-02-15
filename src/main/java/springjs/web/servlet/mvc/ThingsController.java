e8be8c2dfbc1007c5a1ad43157e3a80ead7fb953
Lorenzo Dee <lorenzo.dee@orangeandbronze.com>
2022-02-15 13:06:49 +0800

    Disconnect from base controller class

--- src/main/java/springjs/web/servlet/mvc/ThingsController.java
+++ src/main/java/springjs/web/servlet/mvc/ThingsController.java
@@ -1,11 +1,11 @@
 package springjs.web.servlet.mvc;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -14,8 +14,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import springjs.domain.model.Thing;
@@ -23,138 +25,101 @@ import springjs.domain.model.ThingRepository;
 
 @Controller
 @RequestMapping("/things")
-public class ThingsController extends BaseController {
+class ThingsController {
 
 	/**
 	 * Regular expression in the request mapping to accept UUID as identifier.
 	 */
 	// @formatter:off
 	private static final String ID_REGEX_UUID = "/{id:"
-			+ "[0-9a-fxA-FX]{8}-[0-9a-fxA-FX]{4}-[0-9a-fxA-FX]{4}-"
-			+ "[0-9a-fxA-FX]{4}-[0-9a-fxA-FX]{12}}";
+			+ "[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}}";
 	// @formatter:on
 
-	private ThingRepository thingRepository;
-
 	@Autowired
-	public ThingsController(ThingRepository projectRepository) {
-		super("things");
-		this.thingRepository = projectRepository;
-	}
+	private ThingRepository thingRepository;
+//	@Autowired
+//	private MessageSource messageSource;
 
-	@Index
+	@GetMapping
 	String index(Pageable pageable, Model model) {
 		model.addAttribute("thingsPage", thingRepository.findAll(pageable));
-		return indexViewName;
+		return "things/index";
 	}
 
-	@Show(path = ID_REGEX_UUID)
+	@GetMapping(path = ID_REGEX_UUID)
 	String show(@PathVariable UUID id, Model model) {
-		Optional<Thing> thing = thingRepository.findById(id);
-		if (!thing.isPresent()) {
-			throw new NotFoundException(id);
-		}
-		model.addAttribute("thing", thing.get());
-		return showViewName;
+		Thing thing = thingRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		model.addAttribute("thing", thing);
+		return "things/show";
 	}
 
-	@Create
+	@GetMapping(params = "create")
 	String create(Model model) {
 		model.addAttribute("thing", new Thing());
-		return createViewName;
+		prepareFormModels(model);
+		return "things/create";
 	}
 
-	private boolean saveOrUpdate(Thing thing, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return false;
-		}
-		thing = thingRepository.save(thing);
-		return true;
-	}
-
-	@Save
+	@PostMapping
 	String save(@Valid Thing thing, BindingResult bindingResult,
 			Model model, RedirectAttributes redirectAttributes) {
-		if (!saveOrUpdate(thing, bindingResult)) {
-			// prepareCreateForm(model);
-			return getCreateViewName();
-		}
-		redirectAttributes.addAttribute("id", thing.getId());
-		redirectAttributes.addFlashAttribute("_notice", "Thing was successfully created.");
-		return redirectToIndex;
-	}
-
-	@Save(produces = "text/javascript")
-	String saveViaAjax(@Valid Thing thing, BindingResult bindingResult, Model model) {
-		if (!saveOrUpdate(thing, bindingResult)) {
-			// prepareCreateForm(model);
-			return getCreateViewName();
+		if (bindingResult.hasErrors()) {
+			prepareFormModels(model);
+			return "things/create";
 		}
 		thing = thingRepository.save(thing);
-		return showViewName;
+		// @formatter:off
+		/*
+		redirectAttributes.addFlashAttribute("message",
+				this.messageSource.getMessage("thing.created",
+						new Object[] { thing.getId() }, locale));
+		*/
+		// @formatter:on
+		return "redirect:/things";
 	}
 
-	@Edit(path = ID_REGEX_UUID)
+	@GetMapping(path = ID_REGEX_UUID, params = "edit")
 	String edit(@PathVariable UUID id, Model model) {
-		Optional<Thing> thing = thingRepository.findById(id);
-		if (!thing.isPresent()) {
-			throw new NotFoundException(id);
-		}
-		model.addAttribute("thing", thing.get());
-		return editViewName;
+		Thing thing = thingRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		model.addAttribute("thing", thing);
+		prepareFormModels(model);
+		return "things/edit";
 	}
 
-	@Update(path = ID_REGEX_UUID)
+	@PutMapping(path = ID_REGEX_UUID)
 	String update(@PathVariable UUID id, @Valid Thing thing, BindingResult bindingResult,
 			Model model, RedirectAttributes redirectAttributes) {
 		thing.setId(id);
-		if (!saveOrUpdate(thing, bindingResult)) {
-			// prepareEditForm(model);
-			return editViewName;
+		if (bindingResult.hasErrors()) {
+			prepareFormModels(model);
+			return "things/edit";
 		}
-		redirectAttributes.addFlashAttribute("_notice", "Thing was successfully updated");
-		return redirectToIndex;
-	}
-
-	@Update(path = ID_REGEX_UUID, produces = "text/javascript")
-	String updateViaAjax(@PathVariable UUID id, @Valid Thing thing, BindingResult bindingResult, Model model) {
-		thing.setId(id);
-		if (!saveOrUpdate(thing, bindingResult)) {
-			// prepareEditForm(model);
-			return editViewName;
-		}
-		return showViewName;
-	}
-
-	@GetMapping(path = ID_REGEX_UUID, params = { "delete" })
-	String deleteConfirm(@PathVariable UUID id, Model model) {
-		Optional<Thing> thing = thingRepository.findById(id);
-		if (!thing.isPresent()) {
-			throw new NotFoundException(id);
-		}
-		model.addAttribute("thing", thing.get());
-		return deleteViewName;
+		thing = thingRepository.save(thing);
+		// @formatter:off
+		/*
+		redirectAttributes.addFlashAttribute("message",
+				this.messageSource.getMessage("thing.updated",
+						new Object[] { thing.getId() }, locale));
+		*/
+		// @formatter:on
+		return "redirect:/things";
 	}
 
 	@DeleteMapping(path = ID_REGEX_UUID)
 	String delete(@PathVariable UUID id) {
-		thingRepository.deleteById(id);
-		return redirectToIndex;
-	}
-
-	@DeleteMapping(path = ID_REGEX_UUID, produces = "text/javascript")
-	String deleteViaAjax(@PathVariable UUID id, Model model) {
-		thingRepository.deleteById(id);
-		model.addAttribute("id", id);
-		return deleteViewName;
-	}
-
-	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such thing")
-	@SuppressWarnings("serial")
-	public static class NotFoundException extends RuntimeException {
-		public NotFoundException(UUID id) {
-			super("Thing [" + id + "] not found");
+		try {
+			this.thingRepository.deleteById(id);
+			return "redirect:/things";
 		}
+		catch (EmptyResultDataAccessException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	void prepareFormModels(Model model) {
+		// Provide models to support the create/edit <form> (e.g. drop lists)
 	}
 
 }
